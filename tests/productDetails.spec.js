3f534859d6f98bb5a6156d5dbe0f379cf2ed4abb
Joao Moreira <jgmoreira1809@gmail.com>
2022-05-12 11:14:46 -0700

    requisito 6 quase pronto

--- tests/productDetails.spec.js
+++ tests/productDetails.spec.js
@@ -31,13 +31,20 @@ const productDetails = require('../src/productDetails');
 
 describe('6 - Implemente os casos de teste para a função `productDetails`', () => {
   it('Verifica se a função `productDetails` tem o comportamento esperado', () => {
-    fail('Teste vazio!');
     // ESCREVA SEUS TESTES ABAIXO:
     // Teste se productDetails é uma função.
+    expect(typeof productDetails).toBe('function');
     // Teste se o retorno da função é um array.
+    expect(typeof productDetails()).toBe('array');
     // Teste se o array retornado pela função contém dois itens dentro.
+    expect(productDetails().length).toEqual(2);    
     // Teste se os dois itens dentro do array retornado pela função são objetos.
+    expect(typeof Object.entries(productDetails())).toBe('object');
     // Teste se quando passado parâmetros diferentes entre si, os dois objetos também são diferentes entre si.
+    expect(productDetails('fistProduct', 'secondProduct')[0] !== productDetails('fistProduct', 'secondProduct')[1]).toEqual(true);
     // Teste se os dois productIds terminam com 123.
+    expect(productDetails()[0].details.productId).toMatch(/123/);
+    expect(productDetails()[1].details.productId).toMatch(/123/);
+    // https://archive.jestjs.io/docs/pt-br/expect#tomatchregexp--string
   });
 });
